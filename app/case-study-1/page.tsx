968bee8313d393171e1754581e124b39399e8278
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-02-24 18:34:29 +0530

    Added code for animation in the final page

--- app/case-study-1/page.tsx
+++ app/case-study-1/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
+
 import Link from 'next/link'
+import { motion } from "motion/react"
 
 const page = () => {
     return (
         <div>
+            <motion.div
+                initial={{
+                    width: 100,
+                    height: 50,
+                    top: 480,
+                    left: 240
+                }}
+                animate={{
+                    width: "100vw",
+                    height: "75vh",
+                    top: 0,
+                    left: 0
+                }}
+                transition={{ duration: 5 }}
+                className='bg-red-500 h-50 w-100 cursor-pointer absolute top-120 left-60'
+            >
+            </motion.div>
             <h1>
                 Case Study 1
             </h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
+'use client'
+
 import Link from "next/link";
+import { motion } from "motion/react"
+import { useState } from "react";
 
 export default function Home() {
+  const [expanded, setExpanded] = useState(false)
   return (
     <div>
       <h1>
-        Lorem Ipsum
+        Aman's Portfolio
       </h1>
 
-      <Link href="/case-study-1">Case Study 1</Link>
-      <Link href="/case-study-2">Case Study 2</Link>
+      <div>
+        <Link href="/case-study-1">Case Study 1</Link>
+        <Link href="/case-study-2">Case Study 2</Link>
+      </div>
+
+      <div className="flex gap-16">
+        <motion.div
+          layout
+          onClick={() => setExpanded(!expanded)}
+          animate={{
+            width: expanded ? "100vw" : 150,
+            height: expanded ? "50vh" : 150,
+          }}
+          transition={{ duration: 5 }}
+          className="bg-red-500 w-150 h-100 rounded-2xl cursor-pointer"
+        >
+        </motion.div>
+        <div className="bg-blue-500 w-150 h-100 rounded-2xl cursor-pointer"></div>
+      </div>
     </div>
   );
 }
