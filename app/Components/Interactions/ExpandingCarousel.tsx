3c8f1c3efbdcd3da94cb187b0f1efc00e2c1e248
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-03-03 10:19:56 +0530

    Created looping carousel

--- app/Components/Interactions/ExpandingCarousel.tsx
+++ app/Components/Interactions/ExpandingCarousel.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+'use client'
+
+import { motion } from "framer-motion"
+
+const EachCard = ({ title }: { title: string }) => {
+    return (
+        <div className='bg-black text-white min-w-[200px] p-4 rounded-md h-40 cursor-pointer'>{title}</div>
+    )
+}
 
 const ExpandingCarousel = () => {
+    const cards = [
+        { title: "Card #1" },
+        { title: "Card #2" },
+        { title: "Card #3" },
+        { title: "Card #4" },
+    ]
+
     return (
-        <div>ExpandingCarousel</div>
+        <div className="bg-blue-200 w-screen h-screen text-black p-4 flex items-center">
+            <div className="bg-white w-full overflow-hidden rounded-md p-4">
+                <motion.div
+                    className="flex gap-2 w-max"
+                    animate={{ x: ["0%", "-50%"] }}
+                    transition={{
+                        ease: "linear",
+                        duration: 12,
+                        repeat: Infinity,
+                    }}
+                >
+                    {cards.map((card, i) => (
+                        <EachCard key={i} title={card.title} />
+                    ))}
+                    {cards.map((card, i) => (
+                        <EachCard key={`duplicate-${i}`} title={card.title} />
+                    ))}
+                </motion.div>
+            </div>
+        </div>
     )
 }
 
