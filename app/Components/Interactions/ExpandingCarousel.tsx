e3d0e1fbba45e0da013c46b44331ef6e5d1effa9
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-03-03 09:54:07 +0530

    Created ExpandingCarousel component

---
+++ app/Components/Interactions/ExpandingCarousel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpandingCarousel = () => {
+    return (
+        <div>ExpandingCarousel</div>
+    )
+}
+
+export default ExpandingCarousel
--- app/interaction-library/page.tsx
+++ app/interaction-library/page.tsx
@@ -1,9 +1,10 @@
 import RadialRevealCard from '../Components/Interactions/RadialRevealCard'
 import ExpandingCard from '../Components/Interactions/ExpandingCard'
+import ExpandingCarousel from '../Components/Interactions/ExpandingCarousel'
 
 const page = () => {
     return (
-        <ExpandingCard />
+        <ExpandingCarousel />
     )
 }
 
