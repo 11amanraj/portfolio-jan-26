515b3dbe41696bf5e4bfa0f0c49d3f1eff46040e
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-03-07 12:28:36 +0530

    Made some changes in the content

--- app/case-study-2/page.tsx
+++ app/case-study-2/page.tsx
@@ -159,27 +159,6 @@ function CollapsedCard() {
                     </div>
                 </motion.div>
                 <motion.div variants={item} className='bg-[#E1F0F2] rounded-lg px-8 py-8 flex-1 flex flex-col gap-4 text-black'>
-                    <h3 className="text-[32px] font-heading font-bold">INSIGHTS SUMMARY</h3>
-                    <div className='flex flex-col gap-6'>
-                        <div className='flex flex-col'>
-                            <h4 className='font-semibold'>Transaction Overload</h4>
-                            <p>Frequent micro-payments make it difficult for users to track spending. Small daily expenses accumulate unnoticed, and many rely on bank balance.</p>
-                        </div>
-
-                        <div className='flex flex-col'>
-                            <h4 className='font-semibold'>Fragmented Expense Visibility</h4>
-                            <p>Transaction histories lack clarity and consistency. Merchant names vary across POS systems, making it harder for users to recognize spending.</p>
-                        </div>
-
-                        <div className='flex flex-col'>
-                            <h4 className='font-semibold'>Friction in Shared Expenses</h4>
-                            <p>Splitting bills introduces extra steps and limited visibility. Users struggle to track who has paid, while pending obligations often disappear.</p>
-                        </div>
-                    </div>
-                </motion.div>
-            </div>
-            <div className='flex-1 flex flex-col gap-4'>
-                <motion.div variants={item} className='bg-[#E1F0F2] rounded-lg px-8 py-8 flex-1 flex flex-col gap-4 text-black'>
                     <h3 className="text-[32px] font-heading font-bold">DESIGN OPPORTUNITIES</h3>
                     <div className='flex flex-col gap-6'>
                         <div className='flex flex-col'>
@@ -195,6 +174,27 @@ function CollapsedCard() {
                         <div className='flex flex-col'>
                             <h4 className='font-semibold'>Integrated Social Payments</h4>
                             <p>Make shared expenses easier to manage within everyday transactions. Pending balances and repayments should remain visible.</p>
+                        </div>
+                    </div>
+                </motion.div>
+            </div>
+            <div className='flex-1 flex flex-col gap-4'>
+                <motion.div variants={item} className='bg-[#E1F0F2] rounded-lg px-8 py-8 flex-1 flex flex-col gap-4 text-black'>
+                    <h3 className="text-[32px] font-heading font-bold">INSIGHTS SUMMARY</h3>
+                    <div className='flex flex-col gap-6'>
+                        <div className='flex flex-col'>
+                            <h4 className='font-semibold'>Transaction Overload</h4>
+                            <p>Frequent micro-payments make it difficult for users to track spending. Small daily expenses accumulate unnoticed, and many rely on bank balance.</p>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <h4 className='font-semibold'>Fragmented Expense Visibility</h4>
+                            <p>Transaction histories lack clarity and consistency. Merchant names vary across POS systems, making it harder for users to recognize spending.</p>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <h4 className='font-semibold'>Friction in Shared Expenses</h4>
+                            <p>Splitting bills introduces extra steps and limited visibility. Users struggle to track who has paid, while pending obligations often disappear.</p>
                         </div>
                     </div>
                 </motion.div>
@@ -226,12 +226,12 @@ const page = () => {
             <div className='mx-6 sm:mx-8 md:mx-16 2xl:mx-80 self-center'>
                 <TitleSection />
 
-                <ContentSection title="Problem">
+                <ContentSection title="Introduction">
                     <p className='font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
                 </ContentSection>
 
-                <CollapsibleSection collapsed={<CollapsedCard />} expanded={<ExpandedCard />} title='Research' />
+                <CollapsibleSection collapsed={<CollapsedCard />} expanded={<ExpandedCard />} title='Understanding the Problem' />
 
                 <ContentSection title="Definition">
                     <p className='font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
