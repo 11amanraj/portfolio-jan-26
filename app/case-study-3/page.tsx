8e8bafb3775c97074935f5370003a1957abe466e
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-17 11:56:08 +0530

    V0.5 - Error Fix

--- app/case-study-3/page.tsx
+++ app/case-study-3/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React from 'react'
+import { Suspense } from 'react'
 import { motion } from 'motion/react'
 import { useSearchParams } from "next/navigation";
 
-const page = () => {
+const CaseStudy3Content = () => {
     const params = useSearchParams()
     const id = params.get("id")
 
@@ -25,6 +25,14 @@ const page = () => {
                 <p>Is this working?</p>
             </motion.div>
         </div>
+    )
+}
+
+const page = () => {
+    return (
+        <Suspense>
+            <CaseStudy3Content />
+        </Suspense>
     )
 }
 
