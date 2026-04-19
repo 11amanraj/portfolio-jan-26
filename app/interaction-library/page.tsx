f85b700c586c5aaa101c9488fc3aeb2aac3286db
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-19 14:00:41 +0530

    Done with xl breakpoint

--- app/interaction-library/page.tsx
+++ app/interaction-library/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
     return (
-        <div className="bg-green-500 h-screen w-screen flex">
-            <div className="bg-pink-500 w-26 shrink-10"></div>
+        <div className="bg-green-500 h-screen w-screen flex flex-col 2xl:flex-row">
+            <div className="w-26 shrink-10 hidden 2xl:block"></div>
 
-            <div className="flex-1 shrink min-w-368 bg-black relative font-heading flex gap-12 flex-col py-12 pl-16">
+            <div className="flex-1 shrink 2xl:min-w-368 bg-black relative font-heading flex gap-12 flex-col py-12 pl-8 pr-8 2xl:pl-16 2xl:pr-0">
                 <div
                     className="text-white font-extralight text-[160px] leading-none select-none pointer-events-none"
                 >
@@ -32,10 +32,9 @@ export default function Page() {
 
             </div>
 
-            <div className="bg-blue-400 w-31 relative flex items-end justify-center py-12 min-w-16">
+            <div className="bg-blue-400 w-full 2xl:w-31 relative flex items-center justify-end 2xl:items-end 2xl:justify-center py-12 min-w-16 h-20 2xl:h-auto pr-8 2xl:pr-0">
                 <span
-                    className="text-[#535353] font-bold uppercase text-2xl tracking-[2.64px] whitespace-nowrap"
-                    style={{ writingMode: 'vertical-rl' }}
+                    className="text-[#535353] font-bold uppercase text-2xl tracking-[2.64px] whitespace-nowrap label-writing-mode"
                 >
                     Interaction Library
                 </span>
