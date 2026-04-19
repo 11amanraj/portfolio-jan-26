d26f8f5596e0d6cfdfdd31dd3b37c90601418188
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-19 13:31:23 +0530

    Completed 1536px width

--- app/interaction-library/page.tsx
+++ app/interaction-library/page.tsx
@@ -1,10 +1,9 @@
 export default function Page() {
     return (
         <div className="bg-green-500 h-screen w-screen flex">
-
             <div className="bg-pink-500 w-26 shrink-10"></div>
 
-            <div className="flex-1 shrink min-w-384 bg-black relative font-heading flex gap-12 flex-col py-12 pl-16 pr-31">
+            <div className="flex-1 shrink min-w-368 bg-black relative font-heading flex gap-12 flex-col py-12 pl-16">
                 <div
                     className="text-white font-extralight text-[160px] leading-none select-none pointer-events-none"
                 >
@@ -31,20 +30,15 @@ export default function Page() {
                     </div>
                 </div>
 
-                <div
-                    className="absolute flex items-center justify-center w-31"
-                    style={{
-                        right: 0,
-                        bottom: 48,
-                    }}
+            </div>
+
+            <div className="bg-blue-400 w-31 relative flex items-end justify-center py-12 min-w-16">
+                <span
+                    className="text-[#535353] font-bold uppercase text-2xl tracking-[2.64px] whitespace-nowrap"
+                    style={{ writingMode: 'vertical-rl' }}
                 >
-                    <span
-                        className="text-[#535353] font-bold uppercase text-2xl tracking-[2.64px] whitespace-nowrap"
-                        style={{ writingMode: 'vertical-rl' }}
-                    >
-                        Interaction Library
-                    </span>
-                </div>
+                    Interaction Library
+                </span>
             </div>
         </div>
     )
