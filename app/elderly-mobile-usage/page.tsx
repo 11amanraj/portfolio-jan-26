496c287d84b362f0f9793ef44a82f59b06f1a442
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-21 00:08:11 +0530

    Added container div for main content

--- app/elderly-mobile-usage/page.tsx
+++ app/elderly-mobile-usage/page.tsx
@@ -1,6 +1,14 @@
 const page = () => {
     return (
-        <div>page</div>
+        <div>
+            <div className='relative bg-red-500 w-screen aspect-[2.65] object-contain overflow-hidden'>
+                <img src="\project-thumbnails\elderly-ux.jpg" alt="elderly-mobile-usage-thumbnail" />
+            </div>
+
+            <div className="bg-blue-700 h-500 max-w-304 mx-auto">
+                A
+            </div>
+        </div>
     )
 }
 
