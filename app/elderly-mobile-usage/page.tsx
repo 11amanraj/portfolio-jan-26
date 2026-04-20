caf33818f73db975a80c209d78e58953b29245d7
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-21 00:32:40 +0530

    Added sections to the page

--- app/elderly-mobile-usage/page.tsx
+++ app/elderly-mobile-usage/page.tsx
@@ -1,14 +1,32 @@
+import ContentSection from "../Components/CaseStudy/ContentSection"
+import TitleSection from "../Components/CaseStudy/TitleSection"
+import Footer from "../Components/Footer/Footer"
+
 const page = () => {
     return (
-        <div>
-            <div className='relative bg-red-500 w-screen aspect-[2.65] object-contain overflow-hidden'>
-                <img src="\project-thumbnails\elderly-ux.jpg" alt="elderly-mobile-usage-thumbnail" />
-            </div>
+        <>
+            <div>
+                <div className='relative bg-red-500 w-screen h-[60vh] md:h-[75vh] object-cover overflow-hidden'>
+                    <img src="\project-thumbnails\elderly-ux.jpg" alt="elderly-mobile-usage-thumbnail" />
+                </div>
 
-            <div className="bg-blue-700 h-500 max-w-304 mx-auto">
-                A
+                <div className="max-w-304 mx-4 md:mx-8 xl:mx-auto">
+                    <TitleSection />
+
+                    <ContentSection title="Introduction">
+                        <p className='font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                    </ContentSection>
+
+                    <ContentSection title="Solution">
+                        <div className='bg-white w-full h-244'></div>
+                    </ContentSection>
+
+                    <div className='my-8 h-px w-full bg-[#414651]'></div>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
