c55f0d5be536c8c192ecf5e73e7289eff114e3e2
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-04-20 17:10:42 +0530

    Updated the NavBar design

--- app/Components/Navigation/NavBar.tsx
+++ app/Components/Navigation/NavBar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
+
+import { usePathname } from 'next/navigation'
+import ArrowBack from '@mui/icons-material/ArrowBack'
 
 const NavBar = () => {
-  return (
-    <nav className='flex justify-between py-8 responsive-padding'>
-        <img src='/icons/website_logo.svg' alt='website-logo' className='w-8'/>
+  const pathname = usePathname()
+
+  if (pathname === '/') {
+    return (
+      <nav className='flex justify-between py-8 responsive-padding'>
+        <img src='/icons/website_logo.svg' alt='website-logo' className='w-8' />
         <a
           href='/aman_resume.pdf'
           download
@@ -11,6 +17,15 @@ const NavBar = () => {
         >
           Resume
         </a>
+      </nav>
+    )
+  }
+
+  return (
+    <nav className='fixed top-0 left-0 z-50 flex justify-between py-8 responsive-padding'>
+      <a href='/' className='cursor-pointer bg-white/70 border border-black rounded-full p-2'>
+        <ArrowBack sx={{ color: 'black', fontSize: 48 }} />
+      </a>
     </nav>
   )
 }
