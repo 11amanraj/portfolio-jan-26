f8c93724dd21d7a1e0abac82c78248ae4b9f046e
11amanraj <110328977+11amanraj@users.noreply.github.com>
2026-05-15 14:58:35 +0530

    Made rendering of back button responsive

--- app/Components/Navigation/NavBar.tsx
+++ app/Components/Navigation/NavBar.tsx
@@ -66,7 +66,7 @@ const NavBar = () => {
       {!isHomePage && (
         <Link
           href="/"
-          className="fixed left-4 top-24 z-50 inline-flex h-[51px] items-center gap-2 border border-[#2e2e2e] bg-[#080808]/45 px-5 text-[18px] text-[#dbdbdb] backdrop-blur-md transition-colors hover:border-[#68acef] hover:text-white sm:left-12"
+          className="fixed left-4 top-24 z-50 hidden h-[51px] items-center gap-2 border border-[#2e2e2e] bg-[#080808]/45 px-5 text-[18px] text-[#dbdbdb] backdrop-blur-md transition-colors hover:border-[#68acef] hover:text-white sm:left-12 md:inline-flex"
         >
           <ArrowBackIcon />
           Back
